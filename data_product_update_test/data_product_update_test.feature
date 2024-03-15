3b70cd79e1553d3a3a80b151f133ca0d026f172c
a_liang <t112598014@ntut.edu.tw>
2024-03-15 17:54:18 +0800

    feature: 重構product update 減少重複程式碼

--- data_product_update_test/data_product_update_test.feature
+++ data_product_update_test/data_product_update_test.feature
@@ -21,6 +21,7 @@ Given 已開啟服務dispatcher
 #Scenario
 	Scenario: 使用者使用product update指令更新data product，失敗情境
 	Given 已有data product "<ProductName>"
+	Given 儲存nats現有data product "<ProductName>" 副本
 	When 更新data product "<ProductName>" 註解 "<Description>" "<Enabled>" schema檔案 "<Schema>"
 	Then Cli回傳建立失敗
 	And 應有錯誤訊息 "<error_message>"
--- data_product_update_test/data_product_update_test.go
+++ data_product_update_test/data_product_update_test.go
@@ -1,22 +1,17 @@
 package data_product_update
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"regexp"
-	"strconv"
 	"strings"
+	"test-case/testutils"
 	"testing"
 
 	"github.com/cucumber/godog"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	"github.com/nats-io/nats.go"
 )
 
@@ -27,37 +22,12 @@ type JsonData struct {
 	Schema  interface{} `json:"schema"`
 }
 
-type Config struct {
-	JetstreamURL string `json:"jetstream_url"`
-}
-
-type CmdResult struct {
-	err    error
-	stdout string
-	stderr string
-}
-
-var originJsonData string
 var newJsonData JsonData
-var config Config
-var cmdResult CmdResult
-
-func LoadConfig() error {
-	str, err := os.ReadFile("../config/config.json")
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(str))
-	err = json.Unmarshal([]byte(str), &config)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
+var ut testutils.TestUtils
+var originJsonData string
 
 func TestFeatures(t *testing.T) {
-	LoadConfig()
+	ut.LoadConfig()
 	suite := godog.TestSuite{
 		ScenarioInitializer: InitializeScenario,
 		Options: &godog.Options{
@@ -72,27 +42,8 @@ func TestFeatures(t *testing.T) {
 	}
 }
 
-func ExecuteShell(command string) error {
-	f, err := os.Create("command.sh")
-	f.WriteString(command)
-	defer f.Close()
-
-	cmd := exec.Command("sh", "./command.sh")
-
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	cmdResult.err = cmd.Run()
-
-	cmdResult.stdout = stdout.String()
-	cmdResult.stderr = stderr.String()
-
-	return err
-}
-
 func UpdateDataProductCommand(dataProduct string, description string, enable string, schema string) error {
-	dataProduct = ProcessString(dataProduct)
+	dataProduct = ut.ProcessString(dataProduct)
 	commandString := "../gravity-cli product update "
 	if dataProduct != "[null]" {
 		commandString += dataProduct
@@ -101,7 +52,7 @@ func UpdateDataProductCommand(dataProduct string, description string, enable str
 		if description == "[null]" {
 			commandString += " --desc"
 		} else {
-			description := ProcessString(description)
+			description := ut.ProcessString(description)
 			commandString += " --desc \"" + description + "\""
 		}
 	} else {
@@ -121,15 +72,15 @@ func UpdateDataProductCommand(dataProduct string, description string, enable str
 	if schema != "[ignore]" {
 		commandString += " --schema ./assets/" + schema
 	}
-	commandString += " -s " + config.JetstreamURL
-	ExecuteShell(commandString)
+	commandString += " -s " + ut.Config.JetstreamURL
+	ut.ExecuteShell(commandString)
 
 	return nil
 }
 
 func CreateDateProductCommandSuccess(productName string) error {
-	outStr := cmdResult.stdout
-	productName = ProcessString(productName)
+	outStr := ut.CmdResult.Stdout
+	productName = ut.ProcessString(productName)
 	if outStr == "Product \""+productName+"\" was created\n" {
 		return nil
 	}
@@ -137,33 +88,17 @@ func CreateDateProductCommandSuccess(productName string) error {
 }
 
 func CreateDateProductCommandFail() error {
-	outErr := cmdResult.stderr
-	outStr := cmdResult.stdout
+	outErr := ut.CmdResult.Stderr
+	outStr := ut.CmdResult.Stdout
 	if outStr == "" && outErr != "" {
 		return nil
 	}
 	return errors.New("Cli回傳訊息錯誤")
 }
 
-func ProcessString(s string) string {
-
-	re := regexp.MustCompile(`\[(\S+)\]x(\d+)`)
-	parts := re.FindStringSubmatch(s)
-	if parts == nil {
-		return s
-	}
-	str := parts[1]
-	times, _ := strconv.Atoi(parts[2])
-	completeString := ""
-	for i := 0; i < times; i++ {
-		completeString += str
-	}
-	return completeString
-}
-
 func SearchDataProductByJetstreamSuccess(dataProduct string) error {
-	dataProduct = ProcessString(dataProduct)
-	nc, _ := nats.Connect("nats://" + config.JetstreamURL)
+	dataProduct = ut.ProcessString(dataProduct)
+	nc, _ := nats.Connect("nats://" + ut.Config.JetstreamURL)
 	defer nc.Close()
 
 	js, err := nc.JetStream()
@@ -181,17 +116,6 @@ func SearchDataProductByJetstreamSuccess(dataProduct string) error {
 	return errors.New("jetstream裡未創建成功")
 }
 
-func ClearDataProducts() {
-	nc, _ := nats.Connect("nats://" + config.JetstreamURL)
-	defer nc.Close()
-
-	js, err := nc.JetStream()
-	if err != nil {
-		log.Fatal(err)
-	}
-	js.PurgeStream("KV_GVT_default_PRODUCT")
-}
-
 func AssertErrorMessages(errorMessage string) error {
 	// TODO
 	// outErr := cmdResult.stderr
@@ -202,56 +126,9 @@ func AssertErrorMessages(errorMessage string) error {
 	return nil
 }
 
-func checkNatsService() error {
-	nc, err := nats.Connect("nats://" + config.JetstreamURL)
-	if err != nil {
-		return err
-	}
-	defer nc.Close()
-	return nil
-}
-
-func checkDispatcherService() error {
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return err
-	}
-
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, container := range containers {
-		fmt.Println(container.Names[0])
-		if container.Names[0] == "/gravity-dispatcher" {
-			return nil
-		}
-	}
-	return errors.New("dispatcher container 不存在")
-}
-
-func CreateDataProduct(dataProduct string) error {
-	cmd := exec.Command("../gravity-cli", "product", "create", dataProduct, "-s", config.JetstreamURL)
-	cmd.Run()
-
-	nc, _ := nats.Connect("nats://" + config.JetstreamURL)
-	defer nc.Close()
-
-	js, err := nc.JetStream()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	kv, _ := js.KeyValue("GVT_default_PRODUCT")
-	entry, _ := kv.Get(dataProduct)
-	originJsonData = string(entry.Value())
-	return nil
-}
-
 func DataProductNotChanges(dataProduct string, description string, schema string, enabled string) error {
 
-	nc, _ := nats.Connect("nats://" + config.JetstreamURL)
+	nc, _ := nats.Connect("nats://" + ut.Config.JetstreamURL)
 	defer nc.Close()
 
 	js, err := nc.JetStream()
@@ -269,7 +146,7 @@ func DataProductNotChanges(dataProduct string, description string, schema string
 }
 
 func DataProductUpdateSuccess(dataProduct string, description string, schema string, enabled string) error {
-	nc, _ := nats.Connect("nats://" + config.JetstreamURL)
+	nc, _ := nats.Connect("nats://" + ut.Config.JetstreamURL)
 	defer nc.Close()
 
 	js, err := nc.JetStream()
@@ -322,18 +199,34 @@ func DataProductUpdateSuccess(dataProduct string, description string, schema str
 }
 
 func UpdateSuccess() error {
-	return cmdResult.err
+	return ut.CmdResult.Err
+}
+
+func StoreNowDataProduct(dataProduct string) error {
+	nc, _ := nats.Connect("nats://" + ut.Config.JetstreamURL)
+	defer nc.Close()
+
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kv, _ := js.KeyValue("GVT_default_PRODUCT")
+	entry, _ := kv.Get(dataProduct)
+	originJsonData = string(entry.Value())
+	return nil
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
-		ClearDataProducts()
+		ut.ClearDataProducts()
 		return ctx, nil
 	})
 
-	ctx.Given(`^已開啟服務nats$`, checkNatsService)
-	ctx.Given(`^已開啟服務dispatcher$`, checkDispatcherService)
-	ctx.Given(`^已有data product "([^"]*)"$`, CreateDataProduct)
+	ctx.Given(`^已開啟服務nats$`, ut.CheckNatsService)
+	ctx.Given(`^已開啟服務dispatcher$`, ut.CheckDispatcherService)
+	ctx.Given(`^已有data product "([^"]*)"$`, ut.CreateDataProduct)
+	ctx.Given(`^儲存nats現有data product "([^"]*)" 副本$`, StoreNowDataProduct)
 	ctx.When(`^更新data product "([^"]*)" 註解 "([^"]*)" "([^"]*)" schema檔案 "([^"]*)"$`, UpdateDataProductCommand)
 	ctx.Then(`^data product更改成功$`, UpdateSuccess)
 	ctx.Then(`^Cli回傳建立失敗$`, CreateDateProductCommandFail)
