aa4b14c27a55803a978707d376f671e5407c078d
taroBill <bill890914@gmail.com>
2024-04-19 16:16:32 +0800

    新增pub

--- manual_test_use/create_product_and_ruleset.sh
+++ manual_test_use/create_product_and_ruleset.sh
@@ -6,3 +6,9 @@
     --enabled \
     --event=drinkCreated --method=create \
     --schema="./assets/schema.json"
+
+../gravity-cli pub drinkCreated {"id":1,"name":"test","price":100,"kcal":50}
+../gravity-cli pub drinkCreated {"id":2,"name":"Hi","price":200,"kcal":10}
+../gravity-cli pub drinkCreated {"id":3,"name":"abc","price":300,"kcal":150}
+../gravity-cli pub drinkCreated {"id":4,"name":"sa","price":400,"kcal":1000}
+../gravity-cli pub drinkCreated {"id":5,"name":"sssa","price":500,"kcal":600}
