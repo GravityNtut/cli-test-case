ca4bb66618976f649d1f689943957c18713eb4d1
a_liang <t112598014@ntut.edu.tw>
2024-05-08 17:37:51 +0800

    feature: add data product asset and create step definition

--- data_product_publish_test/data_product_publish_test.feature
+++ data_product_publish_test/data_product_publish_test.feature
@@ -3,3 +3,16 @@ Feature: Data Product publish
 Scenario:
     Given 已開啟服務 nats
     Given 已開啟服務 dispatcher
+
+#Scenario
+    Scenario: 針對data product 的 event publish，成功情境
+    Given 創建 data product "'drink'" 使用參數 "'<DPEnabled>'"
+    Given "'drink'" 創建 ruleset "'drinkEvent'" 使用參數 "'<RSMethod>'" "'<Event>'" "'<RSPk>'" "'<RSHandler>'" "'<RSSchema>'" "'<RSEnabled>'"
+    #When publish 事件使用參數 "'<Event>'" "'<Payload>'"
+    #When 更新 data product "'drink'" 使用參數 --enabled
+    #When 更新 data product "'drink'" 的 ruleset "drinkCreated" 使用參數 --enabled
+    #Then 使用 SDK 查詢 GVT_default_DP_"'drink'" 裡有 "'<Event>'" 帶有 "'<Payload>'"
+    Examples:
+        |   ID   | Event      |                    Payload                   | RSMethod |       RSHandler     |       RSSchema       | RSPk | RSEnabled | DPEnabled |
+        |  M(1)  | drinkEvent | {"id":1,"name":"test","price":100,"kcal":50} | init     | ./assets/handler.js | ./assets/schema.json | id   | [true]    | [true]    |
+        #|  M(2)  | drinkEvent | '{"id":1,"name":"test"}'                       | init     | ./assets/handler.js | ./assets/schema.json | id   | [true]    | [true]    |
--- data_product_publish_test/data_product_publish_test.go
+++ data_product_publish_test/data_product_publish_test.go
@@ -2,6 +2,9 @@ package dataproductpublish
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"os/exec"
 	"test-case/testutils"
 	"testing"
 
@@ -29,6 +32,24 @@ func TestFeatures(t *testing.T) {
 	}
 }
 
+func CreateDataProductRuleset(dataProduct string, ruleset string, RSMethod string, event string, RSPk string, RSHandler string, RSSchema string, RSEnabled string) error {
+
+	if RSEnabled == testutils.TrueString {
+		RSEnabled = "true"
+	} else if RSEnabled == testutils.FalseString {
+		RSEnabled = "false"
+	} else {
+		return errors.New("Enable 必須要[true] 或 [false]")
+	}
+	cmd := exec.Command(testutils.GravityCliString, "product", "ruleset", "add", dataProduct, ruleset, "--event", event, "--method", RSMethod, "--pk", RSPk, "--handler", RSHandler, "--schema", RSSchema, "--enabled="+RSEnabled, "-s", ut.Config.JetstreamURL)
+	fmt.Println(cmd)
+	err := cmd.Run()
+	if err != nil {
+		return errors.New("data product add ruleset failed")
+	}
+	return nil
+}
+
 func InitializeScenario(ctx *godog.ScenarioContext) {
 
 	ctx.Before(func(ctx context.Context, _ *godog.Scenario) (context.Context, error) {
@@ -37,5 +58,6 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	})
 	ctx.Given(`^已開啟服務 nats$`, ut.CheckNatsService)
 	ctx.Given(`^已開啟服務 dispatcher$`, ut.CheckDispatcherService)
-
+	ctx.Given(`^創建 data product "'(.*?)'" 使用參數 "'(.*?)'"`, ut.CreateDataProduct)
+	ctx.Given(`^"'(.*?)'" 創建 ruleset "'(.*?)'" 使用參數 "'(.*?)'" "'(.*?)'" "'(.*?)'" "'(.*?)'" "'(.*?)'" "'(.*?)'"`, CreateDataProductRuleset)
 }
