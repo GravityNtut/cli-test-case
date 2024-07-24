d038f73605ca94c36be133bb0472f00a825393fd
LittleRed945 <erwinis001@gmail.com>
2024-07-24 13:49:38 +0800

    feature: implement two scenario in both go and feature file

--- data_product_purge_test/data_product_purge_test.feature
+++ data_product_purge_test/data_product_purge_test.feature
@@ -7,25 +7,24 @@ Scenario:
 #Scenario: 
 Scenario: Success scenario for the deletion of an existing Data Product
     Given  Create data product with "'<ProductName>'" using parameters "'[true]'"
-    Given Create data product ruleset with "'<ProductName>'", "'drink_ruleset'" using parameters "'[true]'", "'drinkEvent'"
+    Given Create data product ruleset with "'<ProductName>'", "'drink_ruleset'" using parameters "'drinkEvent'", "'[true]'" 
     Given Publish Event "'drinkEvent'" using parameters "''{"id":1,"uid":1,"name":"test","price":100,"kcal":50}''"
-    Then check data product "'<ProductName>'"'s Events amount is 1
-    Then Use NATS JetStream to query the Messages amount of the data product "'<ProductName>'" to be "'1'"	
+    Then Check data product "'<ProductName>'"'s Events amount is "'1'"
+    Then Use NATS JetStream to query the Messages amount of the data product "'<ProductName>'" to be "'1'"
     When Purge data product "'<ProductName>'"
-    Then check data product "'<ProductName>'"'s Events amount is 1
-    Then Using gravity-cli to query "'<ProductName>'" shows it does not exist.
+    Then Check data product "'<ProductName>'"'s Events amount is "'0'"
     Then Use NATS JetStream to query the Messages amount of the data product "'<ProductName>'" to be "'0'"
 Examples:
     |  ID  | ProductName |
     | M(1) | drink       |
 
 #Scenario: 
-Scenario: Fail scenario for the deletion of a non-existent Data Product.
-    When Delete data product "'<ProductName>'"
+Scenario: The purge of a non-existent Data Product.
+    When Purge data product "'<ProductName>'"
     Then CLI returns exit code 1
-	# And The error message should be "'<Error_message>'"
+	And The error message should be "'<Error_message>'"
 Examples:
-    |  ID   | ProductName | Error_message |
-    | E1(1) | failProduct |               |
-    | E2(2) |   [null]    |               |
+    |  ID   | ProductName | Error_message     |
+    | E1(1) | failProduct | product not found |
+    | E2(2) |   [null]    | requires at least 1 arg(s), only received 0 |
     
---
+++ data_product_purge_test/data_product_purge_test.go
@@ -0,0 +1,166 @@
+package dataproductpurge
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"test-case/testutils"
+	"testing"
+
+	"github.com/cucumber/godog"
+	"github.com/nats-io/nats.go"
+)
+
+var ut = testutils.TestUtils{}
+
+func TestFeatures(t *testing.T) {
+	err := ut.LoadConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:        "pretty",
+			Paths:         []string{"./"},
+			StopOnFailure: ut.Config.StopOnFailure,
+			TestingT:      t,
+		},
+	}
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func AddRulesetCommand(dataProduct string, ruleset string, event string, enabled string) error {
+	if enabled == testutils.TrueString {
+		cmd := exec.Command(testutils.GravityCliString, "product", "ruleset", "add", dataProduct, ruleset, "--enabled", "--event", event, "--method", "create", "-s", ut.Config.JetstreamURL)
+		return cmd.Run()
+	} else if enabled == testutils.IgnoreString || enabled == testutils.FalseString {
+		cmd := exec.Command(testutils.GravityCliString, "product", "ruleset", "add", dataProduct, ruleset, "--event", event, "--method", "create", "-s", ut.Config.JetstreamURL)
+		return cmd.Run()
+	}
+	return errors.New("ruleset add 參數錯誤")
+}
+
+func DeleteDataProductCommand(dataProduct string) error {
+	commandString := "../gravity-cli product delete "
+	if dataProduct != testutils.NullString {
+		commandString += dataProduct
+	}
+	commandString += " -s " + ut.Config.JetstreamURL
+	err := ut.ExecuteShell(commandString)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PublishEventCommand(event string, payload string) error {
+	pubString := "../gravity-cli pub " + event + " " + payload
+	if err := ut.ExecuteShell(pubString); err != nil {
+		return err
+	}
+	return nil
+}
+func CheckDataProductEventAmount(dataProduct string, amount string) error {
+	cmd := testutils.GravityCliString + " product list"
+	eventIndex := 3 // Because DESCRIPTION is null
+	err := ut.ExecuteShell(cmd)
+	if err != nil {
+		return err
+	}
+
+	outStr := ut.CmdResult.Stdout
+
+	outStrList := strings.Split(outStr, "\n")
+
+	for _, outStr := range outStrList {
+		if strings.Contains(outStr, dataProduct) {
+			productItems := strings.Fields(outStr)
+
+			if productItems[eventIndex] == amount {
+				return nil
+			}
+
+			return errors.New("CLI returns error message: data product's event amount mismatches")
+		}
+	}
+
+	return errors.New("CLI returns error message: data products doesn't contain the name " + dataProduct)
+}
+
+func CheckDataProductMessagesAmountByJetstream(dataProduct string, amount string) error {
+	nc, _ := nats.Connect(testutils.NatsProtocol + ut.Config.JetstreamURL)
+	defer nc.Close()
+
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streams := js.StreamsInfo()
+
+	for stream := range streams {
+		if stream.Config.Name == "GVT_default_DP_"+dataProduct {
+			msgAmount, _ := strconv.ParseUint(amount, 10, 64)
+			if stream.State.Msgs == msgAmount {
+				return nil
+			}
+
+			return errors.New(strconv.Itoa(int(stream.State.Msgs)))
+		}
+	}
+	return nil
+}
+
+func PurgeDataProduct(productName string) error {
+	commandString := "../gravity-cli product purge "
+	if productName != testutils.NullString {
+		commandString += " " + productName
+	}
+
+	err := ut.ExecuteShell(commandString)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PurgeDataProductFailed() error {
+	if ut.CmdResult.Err != nil {
+		return nil
+	}
+	return errors.New("the data product purge should fail")
+}
+
+func CheckError(errMsg string) error {
+	outStr := ut.CmdResult.Stderr
+	if strings.Contains(outStr, errMsg) {
+		return nil
+	}
+	return errors.New(errMsg)
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+
+	ctx.Before(func(ctx context.Context, _ *godog.Scenario) (context.Context, error) {
+		ut.RestartDocker()
+		return ctx, nil
+	})
+
+	ctx.Given(`^Nats has been opened$`, ut.CheckNatsService)
+	ctx.Given(`^Dispatcher has been opened$`, ut.CheckDispatcherService)
+	ctx.Given(`^Create data product with "'(.*?)'" using parameters "'(.*?)'"$`, ut.CreateDataProduct)
+	ctx.Given(`^Create data product ruleset with "'(.*?)'", "'(.*?)'" using parameters "'(.*?)'", "'(.*?)'"$`, AddRulesetCommand)
+	ctx.Given(`^Publish Event "'(.*?)'" using parameters "'(.*?)'"$`, PublishEventCommand)
+	ctx.Then(`^Check data product "'(.*?)'"'s Events amount is "'(.*?)'"$`, CheckDataProductEventAmount)
+	ctx.Then(`^Use NATS JetStream to query the Messages amount of the data product "'(.*?)'" to be "'(.*?)'"$`, CheckDataProductMessagesAmountByJetstream)
+	ctx.When(`^Purge data product "'(.*?)'"$`, PurgeDataProduct)
+	ctx.Then(`^CLI returns exit code 1$`, PurgeDataProductFailed)
+	ctx.Then(`^The error message should be "'(.*?)'"$`, CheckError)
+	// ctx.Then(`^The error message should be "'(.*?)'"$`, AssertErrorMessages)
+}
