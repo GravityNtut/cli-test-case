9d79661535703ddb0d34b2a01f7ac995f5fdc12f
0A-liang <A8222668@g.pccu.edu.tw>
2024-08-06 15:38:10 +0800

    add tag on data product publish test

--- data_product_list_test/data_product_list_test.go
+++ data_product_list_test/data_product_list_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -13,6 +14,7 @@ import (
 	"time"
 
 	"github.com/cucumber/godog"
+	"github.com/spf13/pflag"
 )
 
 const (
@@ -22,22 +24,28 @@ const (
 
 var ut = testutils.TestUtils{}
 
-func TestFeatures(t *testing.T) {
+var opts = godog.Options{
+	Format:        "pretty",
+	Paths:         []string{"./"},
+	StopOnFailure: ut.Config.StopOnFailure,
+}
+
+func init() {
+	godog.BindCommandLineFlags("godog.", &opts)
+}
+
+func TestMain(m *testing.M) {
+	pflag.Parse()
 	err := ut.LoadConfig()
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 	suite := godog.TestSuite{
 		ScenarioInitializer: InitializeScenario,
-		Options: &godog.Options{
-			Format:        "pretty",
-			Paths:         []string{"./"},
-			StopOnFailure: ut.Config.StopOnFailure,
-			TestingT:      t,
-		},
+		Options:             &opts,
 	}
 	if suite.Run() != 0 {
-		t.Fatal("non-zero status returned, failed to run feature tests")
+		log.Fatal("non-zero status returned, failed to run feature tests")
 	}
 }
 
